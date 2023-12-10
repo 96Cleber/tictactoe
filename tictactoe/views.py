15446dfd7d6fef4237f5088fab6aa2e36eabbe18
96Cleber <cleber.aguado.g@uni.pe>
2023-12-10 16:29:36 -0500

    corregir modelo y salidas

--- tictactoe/views.py
+++ tictactoe/views.py
@@ -22,5 +22,21 @@ def recibe(request,x0,x1,x2,x3,x4,x5,x6,x7,x8):
   modelo_cargado = model_from_json(modelo_cargado_json)
   modelo_cargado.load_weights("tictactoe/modelo.h5")
   modelo_cargado.compile(loss='mean_squared_error', optimizer= 'adam', metrics = 'binary_accuracy')
-  result = modelo_cargado.predict([X])
-  return HttpResponse(result[0])
+  minimo = 1.0
+  soluciones = []
+  for i in range(9):
+    if(X[i]==2):
+      Xcopy = X[:]
+      Xcopy[i] = 0
+      result = modelo_cargado.predict([Xcopy])
+      resultado = float(result[0][0])
+      if(resultado<minimo):
+        minimo = resultado
+        soluciones = []
+        soluciones.append(i)
+      elif(resultado==minimo):
+        soluciones.append(i)
+  if(len(soluciones)==1):
+    return HttpResponse(soluciones)
+  elif(len(soluciones)>1):
+    return HttpResponse(random.choice(soluciones))
